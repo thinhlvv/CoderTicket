991af1a83adb88a4533868b39a7d355eb37a265a
Thịnh Lê <lethinhcdt@gmail.com>
2017-03-04 18:00:21 +0700

    Update form Signin/Signup

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -6,4 +6,18 @@ class EventsController < ApplicationController
   def show
     @event = Event.find(params[:id])
   end
+
+  def create
+
+  end
+
+  def new
+    @event = Event.new()
+  end
+
+  private
+
+    def event_params
+      params.require(:event).permit(:start_at, :end_at, :venue_id, :hero_image_url, :category_id, :name)
+    end
 end
