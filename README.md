c50501dde0a8434336a5d2c7a2071f1b67ef7cd9
Thịnh Lê <lethinhcdt@gmail.com>
2017-03-05 15:19:25 +0700

    implement logic create event

--- README.md
+++ README.md
@@ -39,7 +39,7 @@ The person with best test coverage will win a small present from the instructor 
 
 * [x] User can sign up by providing their email, password, and name.
 * [x] User can login using an email and password.
-* [ ] Users can create events.
+* [x] Users can create events.
 * [ ] Users must click "publish event" before an event becomes viewable to other users.
     * **HINT**: use `published_at:datetime` and `Event.published` scope (class method)
     * Suggestions:
--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -8,7 +8,30 @@ class EventsController < ApplicationController
   end
 
   def create
-    p params
+    new_venue = Venue.new
+    new_venue.name = params[:venues_name]
+    new_venue.full_address = params[:venues_address]
+    new_venue.region_id = params[:region_id]
+    if !new_venue.save!
+      p new_venue.errors.full_messages
+      flash[:error] = "Error occur when adding venue."
+      return
+    end
+
+    new_event = Event.new
+    new_event.name = params[:name]
+    new_event.category_id = params[:category_id]
+    new_event.venue_id = new_venue.id
+    new_event.hero_image_url = params[:image_url]
+    new_event.starts_at = Time.strptime(params[:start_date], "%m/%d/%Y %H:%M %p")
+    new_event.ends_at = Time.strptime(params[:end_date], "%m/%d/%Y %H:%M %p")
+    new_event.extended_html_description = params[:content]
+    if !new_event.save!
+      flash[:error] = "Error occur when adding new event."
+      return
+    end
+
+    flash[:success] = "Create event successfully!"
     redirect_to root_path
   end
 
